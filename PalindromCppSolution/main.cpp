6308d7b458ecae29cca83ed51fdcfc208a1ce673
Gal Berezansky <gberezansky@lightricks.com>
2022-02-25 13:58:19 +0200

    Initial Commit

---
+++ PalindromCppSolution/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PalindromCppSolution
+//
+//  Created by Gal Berezansky on 25/02/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
