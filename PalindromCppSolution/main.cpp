36cd8028e05dc70801c2709a2e62bb1f2b92858e
Ben Yohay <ben@lightricks.com>
2022-02-26 20:45:27 +0200

    Fix overflow by using unsigned long instead of unsigned int.

--- PalindromCppSolution/main.cpp
+++ PalindromCppSolution/main.cpp
@@ -14,7 +14,7 @@
 
 std::vector<unsigned long long> small_pow_10_vector = {};
 
-void intialize_10_pow_vector() {
+void intialize_small_10_pow_vector() {
   // 10^20 doesn't fit into 2^64, so we go until 10^19.
   unsigned long long value = 1;
   for(unsigned long long i = 0; i < 20; i++) {
@@ -66,10 +66,10 @@ inline void printIfDecimalPalindrome(__uint64_t binaryPalindrome) {
   }
 }
 
-inline void checkSmallPalindromes(__uint32_t num) {
+inline void checkSmallPalindromes(unsigned long num) {
   const int num_length = std::floor(std::log2(num)) + 1;
 
-  __uint32_t reversed_num(__rbit(num));
+  unsigned long reversed_num(__rbitll(num) >> 32);
 
   __uint64_t reversedLowerHalf = (__uint64_t)reversed_num >> (32 - num_length);
   __uint64_t palindromeToCheck = ((__uint64_t)num << num_length) | reversedLowerHalf;
@@ -83,41 +83,8 @@ inline void checkSmallPalindromes(__uint32_t num) {
 }
 
 void find_palindroms() {
-  for (__uint32_t i = 1; i < smallPalindromesThreshold; i++) {
+  for (unsigned long i = 1; i < smallPalindromesThreshold; ++i) {
     checkSmallPalindromes(i);
-  }
-
-  for(unsigned long long i = 100000000; i < 1000000000000; i++) {
-    const int binary_length = std::floor(std::log2(i)) + 1;
-
-    std::bitset<64> binary_number(i);
-    std::bitset<64> reversed_binary(__rbitll(i));
-
-    std::bitset<64> concated_even_length_binary = (binary_number << binary_length) |
-                                                  (reversed_binary >> (64 - binary_length));
-
-    std::bitset<64> concated_odd0_length_binary = (binary_number << (binary_length + 1)) |
-                                                  (reversed_binary >> (64 - binary_length));
-
-    std::bitset<64> concated_odd1_length_binary = (binary_number << (binary_length + 1)) |
-                                                  (one << binary_length) |
-                                                  (reversed_binary >> (64 - binary_length));
-
-    if (failFastDecimalPalindrom(concated_even_length_binary.to_ullong())) {
-      counter += 1;
-      std::cout << counter << "\n";
-      std::cout << concated_even_length_binary.to_ullong() << "\n"<< concated_even_length_binary << "\n\n";
-    }
-    if (failFastDecimalPalindrom(concated_odd0_length_binary.to_ullong())) {
-      counter += 1;
-      std::cout << counter << "\n";
-      std::cout << concated_odd0_length_binary.to_ullong() << "\n"<< concated_odd0_length_binary << "\n\n";
-    }
-    if (failFastDecimalPalindrom(concated_odd1_length_binary.to_ullong())) {
-      counter += 1;
-      std::cout << counter << "\n";
-      std::cout << concated_odd1_length_binary.to_ullong() << "\n"<< concated_odd1_length_binary << "\n\n";
-    }
   }
 }
 
@@ -126,7 +93,7 @@ int main(int argc, const char * argv[]) {
 
   auto start = CACurrentMediaTime();
 
-  intialize_10_pow_vector();
+  intialize_small_10_pow_vector();
   find_palindroms();
 
   auto end = CACurrentMediaTime();
