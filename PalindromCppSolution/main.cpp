f868d944afcf1ceaa7536b1732aac7fde5f2b9b1
Ben Yohay <ben@lightricks.com>
2022-02-26 15:01:09 +0200

    Add measurement, extract constant.

--- PalindromCppSolution/main.cpp
+++ PalindromCppSolution/main.cpp
@@ -10,6 +10,8 @@
 #include <arm_acle.h>
 #include <vector>
 
+#import <QuartzCore/QuartzCore.h>
+
 std::vector<unsigned long long> pow_10_vector = {};
 
 void intialize_10_pow_vector() {
@@ -30,14 +32,14 @@ bool inline trivialIsDecimalPalindrom(const unsigned long long l) {
   return l == reversed;
 }
 
-bool inline failFastDecimalPalindrom(const unsigned long long l) {
+bool inline failFastIsDecimalPalindrom(const unsigned long long l) {
     const int decimal_length = std::floor(std::log10(l)) + 1;
     int upper_10_pow = decimal_length - 1;
     int lower_10_pow = 1;
 
-    while(lower_10_pow <= upper_10_pow) {
+    while (lower_10_pow <= upper_10_pow) {
       auto upper_digit = l / pow_10_vector[upper_10_pow] % 10;
-      auto lower_digit = l % pow_10_vector[lower_10_pow] / pow_10_vector[lower_10_pow - 1] ;
+      auto lower_digit = l % pow_10_vector[lower_10_pow] / pow_10_vector[lower_10_pow - 1];
 
       if (upper_digit != lower_digit) {
         return false;
@@ -50,19 +52,12 @@ bool inline failFastDecimalPalindrom(const unsigned long long l) {
     return true;
 }
 
+__uint128_t trivialCheckThreshold = 1000000000;
+
 void find_palindroms() {
-  int counter = 0;
-
-  counter += 1;
-  std::cout << counter << "\n";
-  std::cout << 0 << "\n"<< 0 << "\n\n";
-
-  counter += 1;
-  std::cout << counter << "\n";
-  std::cout << 1 << "\n"<< 1 << "\n\n";
-  std::bitset<128> one(1);
-
-  for(__uint128_t i = 1; i < 100000000; i++) {
+  int counter = 2;
+  
+  for(__uint128_t i = 1; i < trivialCheckThreshold; i++) {
     const int binary_length = std::floor(std::log2(i)) + 1;
 
     std::bitset<128> binary_number(i);
@@ -75,7 +70,7 @@ void find_palindroms() {
                                                   (reversed_binary >> (64 - binary_length));
 
     std::bitset<128> concated_odd1_length_binary = (binary_number << (binary_length + 1)) |
-                                                  (one << binary_length) |
+                                                  (std::bitset<128>(1) << binary_length) |
                                                   (reversed_binary >> (64 - binary_length));
 
     if (trivialIsDecimalPalindrom(concated_even_length_binary.to_ullong())) {
@@ -130,7 +125,14 @@ void find_palindroms() {
 }
 
 int main(int argc, const char * argv[]) {
+  std::cout << 1 << "\n" << 0 << "\n\n" << 2 << "\n" << 1 << "\n\n";
+
+  auto start = CACurrentMediaTime();
+
   intialize_10_pow_vector();
   find_palindroms();
+
+  auto end = CACurrentMediaTime();
+  printf("Time: %f\n", end - start);
   return 0;
 }
